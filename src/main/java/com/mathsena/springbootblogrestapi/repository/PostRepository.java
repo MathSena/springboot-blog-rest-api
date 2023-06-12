548a1a993e8b70d3caa9354eecfc1cb419d291f4
MathSena <MathSena07@hotmail.com>
2023-06-12 18:31:26 -0300

    Creating JPA Repository -PostRepository

---
+++ src/main/java/com/mathsena/springbootblogrestapi/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.mathsena.springbootblogrestapi.repository;
+
+import com.mathsena.springbootblogrestapi.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
