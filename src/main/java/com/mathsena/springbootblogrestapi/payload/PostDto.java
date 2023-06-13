e52fb7ac38bb59c0c89531f29f25407cb5e31d82
MathSena <MathSena07@hotmail.com>
2023-06-13 18:58:20 -0300

    Creating DTO Class - PostDto

---
+++ src/main/java/com/mathsena/springbootblogrestapi/payload/PostDto.java
@@ -0,0 +1,13 @@
+package com.mathsena.springbootblogrestapi.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
+
+}
